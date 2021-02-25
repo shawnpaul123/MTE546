31670e8edaadf3c7479c6aacf656cd9ecaef1382
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2021-02-25 15:21:02 -0500

    Update EKF_SimulatedData.m
    have made changes to accomodate multiple scenarios

--- Lab 2/EKF_SimulatedData.m
+++ Lab 2/EKF_SimulatedData.m
@@ -1,19 +1,41 @@
 close all; clear all;
 %reused EKF code 
 %to accompany for simulated data
+%choose scenario
+scenario = input('which scenario do you want to test for?');
+%scenario is 0,1,2,3,4, or 5
+% 0 - no changes
+% 1- different initial state
+% 2 - different sensor noise
+% 3 - different system noise
+% 4 - different motion model
+% 5 - stationary motion model
 
 
 %% Read in data
-filename = 'Data/simulated_data.csv';
+if scenario == 0 || scenario  == 1
+    filename = 'data/simulated_data.csv'; 
+    truefile = 'data/groundtruth_data.csv';
+else
+    str = num2str(scenario);
+    fn = append(str,'_simulated_data.csv');
+    filename = append('data/',fn);
+    tn = append(str,'_groundtruth.csv');
+    truefile = append('data/',tn);
+    
+end
+    
+
 %need to have data in format of time,ax,ay
 data = readmatrix(filename);
+
 
 t = data(:, 1);
 ax = data(:, 2);
 ay = data(:, 3);
 
 % Read in ground truth data
-truefile = 'Data/groundtruth_data.csv';
+
 data = readmatrix(truefile);
 
 xtrue = data(:, 2);
@@ -26,17 +48,34 @@ N = length(t);
 %% Setup
 plotK = false;
 
+
+
+
+
 % Define models
 A = @(T) [1 0 T 0; ...
           0 1 0 T; ...
           0 0 1 0; ...
           0 0 0 1];
 
+
+
+
 Q = 0.025*eye(4);
-R = [0.21337 0; 0 0.022288];
+R = [0.21337 0; 0 0.022288];%initializing sensor values
+     
+
 
 % Initial conditions
-X0 = [0 0 1 0]';
+if scenario == 1
+    X0 = [10 10 10 10]'; %initializing the positions
+    
+    
+else
+     X0 = [0 0 1 0]'; %initializing the positions
+     
+end
+    
 Pk = eye(4);
 
 % Set up variables
@@ -149,6 +188,7 @@ function Y = h(X)
     y = X(2);
     Y = [(8.3741*x + 0.2395)./(x + 0.0123); ...
          (8.3558*y + 1.3344)./(y + 0.1294)];
+
 end
 
 function J = H(X)
@@ -156,4 +196,5 @@ function J = H(X)
     y = X(2);
     J = [8.3741./(x + 0.0123) - (8.3741*x + 0.2395)./(x + 0.0123).^2, 0, 0, 0;
          0, 8.3558./(y + 0.1294) - (8.3558*y + 1.3344)./(y + 0.1294).^2, 0, 0];
+      
 end
